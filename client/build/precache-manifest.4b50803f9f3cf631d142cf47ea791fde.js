c6c4d591fdc5fe4e65fc408e392aef4e80b81ef3
Gonzalo Huerta Canepa <gonzalo@huerta.cl>
2020-05-19 02:59:38 -0400

    Added build/

---
+++ client/build/precache-manifest.4b50803f9f3cf631d142cf47ea791fde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3671cb0a60e7b2280f356bfac3cf2036",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e86987ce48761af8e21",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "362029eae3f41e3f7553",
+    "url": "/static/css/main.6b4871a3.chunk.css"
+  },
+  {
+    "revision": "8e86987ce48761af8e21",
+    "url": "/static/js/2.f97e0729.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/static/js/2.f97e0729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362029eae3f41e3f7553",
+    "url": "/static/js/main.accd37bc.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
